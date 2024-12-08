a0cb1db8924f8d1f1ef7c295d6a357e6d976b5f4
cvicens <carlos.vicens.alonso@gmail.com>
2024-12-08 15:59:30 +0100

    revamped to be more efficient

--- pipeline/get_images_dataset_component.py
+++ pipeline/get_images_dataset_component.py
@@ -2,6 +2,9 @@ from kfp import compiler
 
 from kfp.dsl import component
 
+BASE_IMAGE="quay.io/modh/runtime-images:runtime-pytorch-ubi9-python-3.9-20241111"
+BOTOCORE_PIP_VERSION= "1.35.54"
+
 # This component downloads the dataset from an S3 bucket and unzips it in the specified volume mount path.
 # The connection to the S3 bucket is created using this environment variables:
 # - AWS_ACCESS_KEY_ID
@@ -14,8 +17,8 @@ from kfp.dsl import component
 # - MODELS_S3_KEY
 # The data is in pickel format and the file name is passed as an environment variable S3_KEY.
 @component(
-    base_image="quay.io/modh/runtime-images:runtime-pytorch-ubi9-python-3.9-20241111",
-    packages_to_install=["boto3", "botocore"]
+    base_image=BASE_IMAGE,
+    packages_to_install=[f"boto3=={BOTOCORE_PIP_VERSION}", f"botocore=={BOTOCORE_PIP_VERSION}"],
 )
 def get_images_dataset(
     images_datasets_root_folder: str, 
@@ -36,9 +39,8 @@ def get_images_dataset(
     region_name = os.environ.get('AWS_DEFAULT_REGION')
     bucket_name = os.environ.get('AWS_S3_BUCKET')
 
-    # Construct and set the IMAGES_DATASET_S3_KEY environment variable
+    # Construct the S3 key for the dataset
     images_dataset_s3_key = f"{images_datasets_root_folder}/{images_dataset_name}.zip"
-
     print(f"images_dataset_s3_key = {images_dataset_s3_key}")
 
     session = boto3.session.Session(
@@ -55,67 +57,55 @@ def get_images_dataset(
 
     bucket = s3_resource.Bucket(bucket_name)
 
-    # TODO don't download the file if it already exists in the volume mount path!!
-    # Create a temporary directory to store the dataset => TODO USE PVC INSTEAD!!!!! CLEAN LATER!!!
-    local_tmp_dir = '/tmp/get_images_dataset'
-    print(f">>> local_tmp_dir: {local_tmp_dir}")
-    
-    # Ensure local_tmp_dir exists
-    if not os.path.exists(local_tmp_dir):
-        os.makedirs(local_tmp_dir)
-
-    # Get the file name from the S3 key
-    file_name = f"{images_dataset_name}.zip"
-    # Download the file
-    local_file_path = f'{local_tmp_dir}/{file_name}'
-
-    # If file doesn't exist in the bucket raise a ValueError
-    objs = list(bucket.objects.filter(Prefix=images_dataset_s3_key))
-    if not any(obj.key == images_dataset_s3_key for obj in objs):
-        raise ValueError(f"File {images_dataset_s3_key} does not exist in the bucket {bucket_name}")
-    
-    print(f"Downloading {images_dataset_s3_key} to {local_file_path}")
-    bucket.download_file(images_dataset_s3_key, local_file_path)
-    print(f"Downloaded {images_dataset_s3_key}")
-
-    # Dataset path
+    # Local temporary directory and dataset folder paths
+    local_tmp_dir = f"{root_mount_path}/tmp"
     images_dataset_path = f"{root_mount_path}/{images_datasets_root_folder}"
-
-    # Ensure dataset path exists
-    if not os.path.exists(images_dataset_path):
-        os.makedirs(images_dataset_path)
-
-    # List the files in the dataset path
-    print(f"Listing files in {images_dataset_path}")
-    print(os.listdir(images_dataset_path))
-
-    # If we haven't unzipped the file yet or we're forced to, unzip it
     images_dataset_folder = f"{images_dataset_path}/{images_dataset_name}"
-    if not os.path.exists(images_dataset_folder) or force_clean:
+
+    # Check if the dataset folder already exists
+    if os.path.exists(images_dataset_folder) and not force_clean:
+        print(f"Dataset folder {images_dataset_folder} already exists. Skipping download and extraction.")
+    else:
+        # Ensure local_tmp_dir exists
+        if not os.path.exists(local_tmp_dir):
+            os.makedirs(local_tmp_dir)
+
+        # Get the file name from the S3 key
+        file_name = f"{images_dataset_name}.zip"
+        local_file_path = f"{local_tmp_dir}/{file_name}"
+
+        # Check if the file exists in S3
+        objs = list(bucket.objects.filter(Prefix=images_dataset_s3_key))
+        if not any(obj.key == images_dataset_s3_key for obj in objs):
+            raise ValueError(f"File {images_dataset_s3_key} does not exist in the bucket {bucket_name}")
+        
+        # Download the file
+        print(f"Downloading {images_dataset_s3_key} to {local_file_path}")
+        bucket.download_file(images_dataset_s3_key, local_file_path)
+        print(f"Downloaded {images_dataset_s3_key}")
+
+        # Ensure dataset path exists
+        if not os.path.exists(images_dataset_path):
+            os.makedirs(images_dataset_path)
+
         # Unzip the file into the images dataset volume mount path
         print(f"Unzipping {local_file_path} to {images_dataset_path}")
-        shutil.unpack_archive(f'{local_file_path}', f'{images_dataset_path}')
+        shutil.unpack_archive(local_file_path, images_dataset_path)
         print(f"Unzipped {local_file_path} to {images_dataset_path}")
 
-        # List the files inside images_dataset_folder folder
-        print(f"Listing files in {images_dataset_folder}")
-        print(os.listdir(images_dataset_folder))
+        # Clean up the temporary zip file
+        os.remove(local_file_path)
 
-    # Locate the YAML file in the dataset folder and replace the path with the actual path
+    # Verify the dataset YAML file exists
     images_dataset_yaml_path = os.path.join(images_dataset_folder, images_dataset_yaml)
-    print(f"images_dataset_yaml_path: {images_dataset_yaml_path}")
-
-    # If the YAML file doesn't exist, raise a ValueError
     if not os.path.exists(images_dataset_yaml_path):
         raise ValueError(f"Dataset YAML file {images_dataset_yaml} not found in {images_dataset_folder}")
 
-    # Replace regex 'path: .*' with 'path: {images_dataset_folder}'
+    # Update the YAML file with the correct dataset path
     with open(images_dataset_yaml_path, 'r') as f:
         data = f.read()
         data = re.sub(r'path: .*', f'path: {images_dataset_folder}', data)
-        # Print the updated YAML file
         print(f"Updated YAML file: {data}")
-        # Write the updated YAML file
         with open(images_dataset_yaml_path, 'w') as f:
             f.write(data)
 
