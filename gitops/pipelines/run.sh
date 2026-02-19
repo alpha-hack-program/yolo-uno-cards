9d948eddfa97d73c03f09bf7de8567f7955b4ad7
cvicens <carlos.vicens.alonso@gmail.com>
2026-02-19 10:49:44 +0100

    New pipeline objects properties

--- gitops/pipelines/run.sh
+++ gitops/pipelines/run.sh
@@ -13,14 +13,25 @@ helm template . --name-template ${ARGOCD_APP_NAME} \
   --set instanceName="${INSTANCE_NAME}" \
   --set dataScienceProjectNamespace=${DATA_SCIENCE_PROJECT_NAMESPACE} \
   --set dataScienceProjectDisplayName=${DATA_SCIENCE_PROJECT_NAMESPACE} \
+  --set partOf="${DATA_SCIENCE_PROJECT_NAMESPACE}" \
   --set mountCaCerts="false" \
+  --set pipelines.enabled="true" \
   --set pipelines.connection.name="pipelines" \
-  --set pipelines.connection.displayName="pipelines" \
+  --set pipelines.connection.displayName="Pipelines S3 Storage" \
+  --set pipelines.connection.description="S3-compatible storage connection for ML pipelines" \
   --set pipelines.connection.type="s3" \
+  --set pipelines.connection.typeProtocol="s3" \
+  --set pipelines.connection.typeRef="s3" \
   --set pipelines.connection.scheme="http" \
+  --set pipelines.connection.host="minio.ic-shared-minio.svc" \
+  --set pipelines.connection.port="9000" \
   --set pipelines.connection.awsAccessKeyId="minio" \
   --set pipelines.connection.awsSecretAccessKey="minio123" \
   --set pipelines.connection.awsDefaultRegion="none" \
   --set pipelines.connection.awsS3Bucket="pipelines" \
-  --set pipelines.connection.awsS3Endpoint="minio.ic-shared-minio.svc:9000" \
+  --set s3.config.awsS3BucketList="pipelines\,models\,datasets" \
+  --set s3.connection.awsAccessKeyId="minio" \
+  --set s3.connection.awsSecretAccessKey="minio123" \
+  --set s3.connection.awsS3Endpoint="http://minio.ic-shared-minio.svc:9000" \
+  --set images.python3="registry.redhat.io/ubi8/python-39:1-158" \
   --include-crds
